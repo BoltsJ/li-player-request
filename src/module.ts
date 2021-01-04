6123db44bc3c9c3ae9c06aaebdcb86c16501cd66
Joe Nosie <joenosie@protonmail.com>
2021-01-04 17:07:30 -0600

    Switch to foundry-vtt-types

--- src/module.ts
+++ src/module.ts
@@ -1,20 +1,14 @@
 // Get the proper type data for the combat tracker
 import { LancerCombat } from "lancer-initiative";
 
-/**
- * TODO: Migrate to a better type repository so that the ignore directives can
- * be removed OR even moar updates to foundry-pc-types T_T
- */
-
 Hooks.once("ready", () => {
   // Only a gm can approve, so only register the socket handler for them
   if (!game.user.isGM) return;
   console.log("li-player-request | Registering socket callback");
 
-  /** @ts-ignore */
   game.socket.on("module.li-player-request", function (data: any) {
     // Get data to pass to the form
-    const combat = game.combats.find((c: LancerCombat) => c.id === data.combat) as LancerCombat;
+    const combat = game.combats.find(c => c.id === data.combat) as LancerCombat;
     const combatant = combat.getCombatant(data.combatant);
     const combatant_name = (combatant.token?.name ?? combatant.name) as string;
     const player_name = game.users.get(data.user)?.name;
@@ -54,7 +48,6 @@ Hooks.on("LancerCombatRequestActivate", (combat: LancerCombat, combatantId: stri
   // send a request to activate to the GM(s)
   console.log("Sending activation request for " + combatantId);
   ui.notifications.info("Sending request to go next.");
-  /** @ts-ignore */
   game.socket.emit("module.li-player-request", {
     scene: combat.scene._id,
     combat: combat.id,
